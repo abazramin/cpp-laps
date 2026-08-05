ef3bdd3ae13c26b3ccceac5515a8301fe1978911
abazramin <azr032024@gmail.com>
2026-08-06 00:45:13 +0300

    sum to numbers

--- Syntax of C++/Main.cpp
+++ Syntax of C++/Main.cpp
@@ -4,19 +4,17 @@ using namespace std;
 
 int main(){
 
-      string yourname;
-      int age;
-      bool ismearid;
-      // Taking input from user
-      cout << "Enter The Name" << endl;
-      cin >> yourname;
-      cout << "Enter your Age" << endl;
-      cin >> age;
-      cout << "Are you a mechatronics engineer? (1 for yes, 0 for no)" << endl;
-      cin >> ismearid;
-      // Displaying the name and age
-      cout << "your Name is "<< yourname << " and your Age is " << age << " and your mechatronics engineer status is " << ismearid << endl;
+      short int a;
+      short int b;
 
+      cout << "Enter two numbers: ";
+      cin >> a >> b;
+
+      cout << "Sum of " << a << " and " << b << " is: " << a + b << endl;
+      cout << "Difference of " << a << " and " << b << " is: " << a - b << endl;
+      cout << "Product of " << a << " and " << b << " is: " << a * b << endl;
+      cout << "Quotient of " << a << " and " << b << " is: " << a / b << endl;
+      cout << "Remainder of " << a << " and " << b << " is: " << a % b << endl;
 
       return 0;
 }
