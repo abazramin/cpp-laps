433416d4265a8bcbea645fa89320d745572c2fed
abazramin <azr032024@gmail.com>
2026-08-07 19:17:59 +0300

    complate the task

--- Syntax of C++/43-SecondstoDaysHoursMinutesSeconds.cpp
+++ Syntax of C++/43-SecondstoDaysHoursMinutesSeconds.cpp
@@ -2,21 +2,29 @@
 #include <cmath>
 using namespace std;
 
-int main(){
-
+int main() {
+      // here we will convert the total secound into days, hours, minutes
       float totalSecound;
+      float reminder;
       cout << "Enter Total Of Secound" << endl;
       cin >> totalSecound;
-
 
       float  SecoundPerDay = (24 * 60 * 60);
       float  SecoundPerHours = (60 * 60);
       float  SecoundPerMinetus = (60);
 
-      float totalDay = totalSecound / SecoundPerDay;
+      float totalDay = floor(totalSecound / SecoundPerDay);
+      reminder = (totalSecound / SecoundPerDay);
 
-      // write code here
+      float totalHours = floor(totalSecound / SecoundPerHours);
+      reminder = reminder / SecoundPerHours;
 
+      float totalMintus = floor(totalSecound / SecoundPerMinetus);
+      reminder = reminder / SecoundPerMinetus;
+
+      float totalSecounds = reminder;
+      
+      cout << totalDay << " : " << totalHours << " : " << totalMintus << " : " << totalSecounds << endl;  
 
       return 0;
 }
