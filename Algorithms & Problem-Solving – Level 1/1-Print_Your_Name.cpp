8e15589cf39830a37a1202c0cb5f73df90b66efa
abazramin <azr032024@gmail.com>
2026-08-16 06:35:10 +0300

    Update comment to clarify purpose of combined solutions in Print_Your_Name program

--- Algorithms & Problem-Solving – Level 1/1-Print_Your_Name.cpp
+++ Algorithms & Problem-Solving – Level 1/1-Print_Your_Name.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 
-
+// we solve the number one and two in same file because they are very simple and easy to understand
 
 void PrintYourName(string name){
       cout << "Your Name is : " << name << endl;
