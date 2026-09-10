4127db42fc562b3f82f3d00c6c4168a3770f8cc8
abazramin <azr032024@gmail.com>
2026-09-10 08:21:08 +0300

    refactor : Poniter

---
+++ C++ Level 2/Pointers/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int sum(int a , int b){
+      return (a + b);
+}
+
+int main(){
+
+      int a = 10;
+
+      cout << "Value of A is " << a << endl;
+      cout << "Address is A " << &a << endl;
+      
+      int * p = &a;
+      
+      *p = 23;
+      
+      cout << "Poniter is " << *p << endl;
+
+      
+      
+      
+      a = 40;
+      
+      cout << "Value of A is " << a << endl;
+      cout << "Poniter is " << *p << endl;
+
+ 
+      return 0;
+}
