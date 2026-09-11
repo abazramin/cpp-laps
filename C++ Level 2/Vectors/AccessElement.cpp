c5924ee08ede51435469cd1a1a5eba032afe69e2
abazramin <azr032024@gmail.com>
2026-09-11 20:50:51 +0300

    (add): c++ level 2

---
+++ C++ Level 2/Vectors/AccessElement.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+      vector<int> nums = {1,2,3,4,5};
+
+
+      nums[1] = 53;
+
+
+
+      // cout << nums[1] * 10 << endl;
+
+
+      // cout << nums.at(4) + 10 << endl;
+      
+
+      nums.at(3) = 20;
+
+
+      // cout << nums.at(3) << endl;
+
+
+
+      vector<int>::iterator itr;
+
+
+      for (itr = nums.begin(); nums.end() != itr; itr++)
+      {
+            cout << *itr << endl;
+      }
+      
+
+      return 0;
+}
